b3d17d5efcc811eb8b21da0c79a63fd39c470e10
Ha Young Jin <teen14y@gmail.com>
2020-01-31 22:31:58 +0900

    MaterialBottomTabNavigator for android

--- src/navigation/MealsNavigator.js
+++ src/navigation/MealsNavigator.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createAppContainer } from "react-navigation";
 import { Platform } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 
 import {
   CategoriesScreen,
@@ -36,39 +37,42 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-const MealfavNavigator = createBottomTabNavigator(
-  {
-    Meals: {
-      screen: MealsNavigator,
-      navigationOptions: {
-        tabBarIcon: tabInfo => {
-          return (
-            <Ionicons
-              name="ios-restaurant"
-              size={30}
-              color={tabInfo.tintColor}
-            />
-          );
-        }
-      }
-    },
-    Favorite: {
-      screen: FavoritesScreen,
-      navigationOptions: {
-        tabBarIcon: tabInfo => {
-          return (
-            <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
-          );
-        }
-      }
+const tabConfig = {
+  Meals: {
+    screen: MealsNavigator,
+    navigationOptions: {
+      tabBarIcon: tabInfo => {
+        return (
+          <Ionicons name="ios-restaurant" size={30} color={tabInfo.tintColor} />
+        );
+      },
+      tabBarColor: Color.primary
     }
   },
+  Favorite: {
+    screen: FavoritesScreen,
+    navigationOptions: {
+      tabBarIcon: tabInfo => {
+        return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
+      },
+      tabBarColor: Color.accent
+    }
+  }
+};
 
-  {
+const MealfavNavigator = Platform.select({
+  ios: createBottomTabNavigator(tabConfig, {
     tabBarOptions: {
       activeTintColor: Color.accent
     }
-  }
-);
+  }),
+  android: createMaterialBottomTabNavigator(tabConfig, {
+    activeColor: "white",
+    shifting: false,
+    barStyle: {
+      backgroundColor: Color.primary
+    }
+  })
+});
 
 export default createAppContainer(MealfavNavigator);
